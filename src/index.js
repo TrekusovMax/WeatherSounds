7dd7da6fb9bc839f18fbc1e7a69f42e82d4b0f83
Maxim <maxtrekusovsite@gmail.com>
2023-02-11 19:00:13 +0300

    sound done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,73 @@
-import "./style.scss"
+import './style.scss'
 
-import { handleInputChange } from "./range"
+const audio = document.getElementById('audio')
+const rangeInputs = document.querySelector('input[type="range"]')
+const body = document.querySelector('body')
+const btn = document.querySelectorAll('.bnt')
 
-const audio = document.getElementById("audio")
-const btn = document.querySelectorAll(".bnt")
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  }
+  const min = target.min
+  const max = target.max
+  const val = target.value
 
-//audio.play()
+  target.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%'
 
-console.log(btn)
+  audio.volume = target.value / 100
+}
+
+rangeInputs.addEventListener('input', handleInputChange)
+
+const weather = {
+  summer: 'summer.mp3',
+  rain: 'rain.mp3',
+  winter: 'winter.mp3',
+}
+const icons = {
+  summer: 'sun.svg',
+  rain: 'cloud-rain.svg',
+  winter: 'cloud-snow.svg',
+}
+let currentWeather = null
+let pause = false
+
+const handleBtnClick = (e) => {
+  //При нажатии на кнопку отображается иконка паузы
+  pause = !pause
+  audio.pause()
+  e.firstElementChild.firstElementChild.setAttribute('href', 'pause.svg')
+  //если нажали на другую кнопку, пауза снимается и меняется иконка другой кнопки
+  if (currentWeather !== e.id) {
+    pause = false
+    currentWeather = weather[e.id].slice(0, -4)
+    //смена аудио
+    audio.src = `./sounds/${weather[currentWeather]}`
+    //смена фона
+    body.style.backgroundImage = `url('./image/${currentWeather}-bg.jpg')`
+  }
+  //перерисовка иконок на кнопках
+  btn.forEach((b) => {
+    if (b.id !== currentWeather) {
+      b.firstElementChild.firstElementChild.setAttribute(
+        'href',
+        `${icons[b.id]}`,
+      )
+    }
+  })
+
+  if (!pause) {
+    audio.play()
+  } else {
+    e.firstElementChild.firstElementChild.setAttribute(
+      'href',
+      `${icons[currentWeather]}`,
+    )
+  }
+}
+
+btn.forEach((b) => {
+  b.addEventListener('click', () => handleBtnClick(b))
+})
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,98 +1,102 @@
-const CopyPlugin = require("copy-webpack-plugin")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const svgToMiniDataURI = require("mini-svg-data-uri")
-const path = require("path")
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const svgToMiniDataURI = require('mini-svg-data-uri')
+const path = require('path')
 
 module.exports = {
-	context: path.resolve(__dirname, "src"),
-	entry: "./index.js",
-	output: {
-		filename: "[name].[contenthash].js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js", ".jsx"],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "public/index.html"),
-		}),
-		new CopyPlugin({
-			patterns: [
-				{
-					from: path.resolve(__dirname, "public/favicon.png"),
-					to: path.resolve(__dirname, "dist"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/icons/sun.svg"),
-					to: path.resolve(__dirname, "dist"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/icons/cloud-rain.svg"),
-					to: path.resolve(__dirname, "dist"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/icons/cloud-snow.svg"),
-					to: path.resolve(__dirname, "dist"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/icons/pause.svg"),
-					to: path.resolve(__dirname, "dist"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/sounds/rain.mp3"),
-					to: path.resolve(__dirname, "dist/sounds"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/sounds/summer.mp3"),
-					to: path.resolve(__dirname, "dist/sounds"),
-				},
-				{
-					from: path.resolve(__dirname, "src/assets/sounds/winter.mp3"),
-					to: path.resolve(__dirname, "dist/sounds"),
-				},
-			],
-		}),
-		new MiniCssExtractPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
-			},
-			{
-				test: /\.svg/,
-				type: "asset/inline",
-				generator: {
-					dataUrl: (content) => {
-						content = content.toString()
-						return svgToMiniDataURI(content)
-					},
-				},
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					{
-						loader: "postcss-loader",
-						options: {
-							postcssOptions: {
-								plugins: [require("postcss-preset-env")],
-							},
-						},
-					},
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/favicon.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/icons/sun.svg'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/icons/cloud-rain.svg'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/icons/cloud-snow.svg'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/icons/pause.svg'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/sounds/rain.mp3'),
+          to: path.resolve(__dirname, 'dist/sounds'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/sounds/summer.mp3'),
+          to: path.resolve(__dirname, 'dist/sounds'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/sounds/winter.mp3'),
+          to: path.resolve(__dirname, 'dist/sounds'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/rain-bg.jpg'),
+          to: path.resolve(__dirname, 'dist/image'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/summer-bg.jpg'),
+          to: path.resolve(__dirname, 'dist/image'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/winter-bg.jpg'),
+          to: path.resolve(__dirname, 'dist/image'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/inline',
+        generator: {
+          dataUrl: (content) => {
+            content = content.toString()
+            return svgToMiniDataURI(content)
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.mp3$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 }
