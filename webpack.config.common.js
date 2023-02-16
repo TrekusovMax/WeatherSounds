134cca70fa2b974ce461e5d02a7407a7148fef38
Trekusov Max <maxtrekusovsite@gmail.com>
2023-02-16 15:29:32 +0300

    refactor

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,7 +1,6 @@
 const CopyPlugin = require("copy-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const svgToMiniDataURI = require("mini-svg-data-uri")
 const path = require("path")
 
 module.exports = {
