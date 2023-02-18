e9175857bfa3141e8b68bb0133cbb5a9d0de64cc
Maxim <maxtrekusovsite@gmail.com>
2023-02-18 19:17:37 +0300

    eslint + typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-unused-vars': 0,
+    '@typescript-eslint/no-explicit-any': ['off'],
   },
 }
+
